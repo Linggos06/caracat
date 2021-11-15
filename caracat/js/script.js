67f704d044918695dd3cd7747bfff6ec465838ef
Linggos06 <julia.snk@hotmail.com>
2021-11-15 19:02:11 +0300

    create sliders

--- caracat/js/script.js
+++ caracat/js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var swiper = new Swiper(".swiper", {
+var swiper1 = new Swiper(".slide_carousel.swiper1", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
@@ -10,8 +10,8 @@ var swiper = new Swiper(".swiper", {
     sticky: true
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
+    nextEl: '.swiper-button-next1',
+    prevEl: '.swiper-button-prev1'
   },
   breakpoints: {
     // when window width is >= 320px
@@ -31,5 +31,33 @@ var swiper = new Swiper(".swiper", {
     el: ".swiper-pagination",
     type: "bullets",
     dynamicBullets: true
+  }
+});
+var swiper2 = new Swiper(".slide_carousel.swiper2", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  speed: 950,
+  freeMode: {
+    enabled: true,
+    sticky: true
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1
+    },
+    // when window width is >= 605px
+    605: {
+      slidesPerView: 1
+    },
+    // when window width is >= 993px
+    993: {
+      slidesPerView: 1
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2'
   }
 });
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".swiper", {
+const swiper1 = new Swiper(".slide_carousel.swiper1", {
     // Optional parameters
     direction: "horizontal",
     loop: true,
@@ -10,8 +10,8 @@ const swiper = new Swiper(".swiper", {
     },
   
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next1',
+      prevEl: '.swiper-button-prev1',
     },
   
   
@@ -35,4 +35,37 @@ const swiper = new Swiper(".swiper", {
       type: "bullets",
       dynamicBullets: true,
     },
+  });
+
+  const swiper2 = new Swiper(".slide_carousel.swiper2", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    speed: 950,
+  
+    freeMode: {
+      enabled: true,
+      sticky: true,
+    },
+
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+      },
+      // when window width is >= 605px
+      605: {
+        slidesPerView: 1,
+      },
+      // when window width is >= 993px
+      993: {
+        slidesPerView: 1,
+      },
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next2',
+      prevEl: '.swiper-button-prev2',
+    },
+  
   });
