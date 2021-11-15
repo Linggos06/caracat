3dcb7e9c351fdab20ad730f449b4c08a5be6f99e
Linggos06 <julia.snk@hotmail.com>
2021-11-16 01:18:00 +0300

    create contacts and advantages parts

--- caracat/js/script.js
+++ caracat/js/script.js
@@ -61,3 +61,31 @@ var swiper2 = new Swiper(".slide_carousel.swiper2", {
     prevEl: '.swiper-button-prev2'
   }
 });
+var swiper3 = new Swiper(".slide_carousel.swiper3", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  speed: 950,
+  freeMode: {
+    enabled: true,
+    sticky: true
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1
+    },
+    // when window width is >= 605px
+    605: {
+      slidesPerView: 1
+    },
+    // when window width is >= 993px
+    993: {
+      slidesPerView: 1
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next3',
+    prevEl: '.swiper-button-prev3'
+  }
+});
